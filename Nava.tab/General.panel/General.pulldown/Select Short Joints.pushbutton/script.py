7e35862a4e7a97cdd77aa882d8993e0081289848
Jose Nava <heureux13@hotmail.com>
2025-11-04 07:43:31 -0600

    added button "Select Short Joints" to the General tab

---
+++ Nava.tab/General.panel/General.pulldown/Select Short Joints.pushbutton/script.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""Copyright (c) 2025 Jose Francisco Nava Perez. All rights reserved.
+
+This code and associated documentation files may not be copied, modified,
+distributed, or used in any form without the prior written permission of
+the copyright holder."""
+
+
+__title__   = "DO NOT PRESS"
+__doc__     = """
+****************************************************************
+Description:
+
+This button is for testin code snippets and ideas before implementing them.
+Odds are it will be constantly changing and not useful, its entire purpose
+is for the author to have a quick button to test whatever code they are working on.
+If you press it could do nothing, throw an error, or change something in your model.
+Once working, it will most likely be moved to a more permanent location.
+
+Current goal fucntion of button is: Selct all short joints.
+****************************************************************
+"""
+
+# Imports
+# ==================================================
+from Autodesk.Revit.DB import *
+from pyrevit import revit, forms, DB
+from Autodesk.Revit.UI import UIDocument
+from Autodesk.Revit.ApplicationServices import Application
+from revit_duct import RevitDuct, JointSize
+from tag_duct import TagDuct
+from revit_element import RevitElement
+
+#.NET Imports
+# ==================================================
+import clr
+clr.AddReference('System')
+from System.Collections.Generic import List
+
+
+# Variables
+# ==================================================
+app   = __revit__.Application           #type: Application
+uidoc = __revit__.ActiveUIDocument      #type: UIDocument
+doc   = revit.doc                       #type: Document
+view  = revit.active_view
+
+ducts = (DB.FilteredElementCollector(doc, view.Id)
+         .OfCategory(DB.BuiltInCategory.OST_FabricationDuctwork)
+         .WhereElementIsNotElementType()
+         .ToElements())
+
+# Main Code
+# ==================================================
+
+ducts   = [RevitDuct(doc, view, el) for el in ducts]
+shorts  = [d for d in ducts if d.is_full_joint == JointSize.SHORT]
+
+RevitElement.select_many(uidoc, shorts)
+forms.alert("Selected {} short joints".format(len(shorts)))
+
+print("It's called a do not press button for a reason, why did you press it?")
