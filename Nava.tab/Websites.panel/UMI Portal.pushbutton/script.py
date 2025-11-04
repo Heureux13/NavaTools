d02e337d538c9d71b067ba4d73ba368572545cd1
Heureux13 <heureux13@hotmail.com>
2025-11-03 21:33:44 -0600

    Portal

---
+++ Nava.tab/Websites.panel/UMI Portal.pushbutton/script.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+__title__   = "UMI Portal"
+__doc__     = """
+****************************************************************
+Description:
+
+Takes you to the fieldwire site, 
+****************************************************************
+"""
+
+import webbrowser
+
+webbrowser.open("https://www.Google.com/")
