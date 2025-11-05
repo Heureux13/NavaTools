d66f7543636c175589e54e47c1523ecc772a568d
Heureux13 <heureux13@hotmail.com>
2025-11-04 18:47:33 -0600

    debugged

--- Nava.tab/Sheetmetal.panel/Select.pulldown/Accessories.pushbutton/script.py
+++ Nava.tab/Sheetmetal.panel/Select.pulldown/Accessories.pushbutton/script.py
@@ -7,19 +7,12 @@ distributed, or used in any form without the prior written permission of
 the copyright holder."""
 # ======================================================================
 
-__title__   = "John's Button"
+__title__   = "Accessories"
 __doc__     = """
 ************************************************************************
 Description:
 
-Current goal fucntion of button is: Select boots per Goolsby request.
-
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-This button is for testin code snippets and ideas before implementing them.
-Odds are it will be constantly changing and not useful, its entire purpose
-is for the author to have a quick button to test whatever code they are working on.
-If you press it could do nothing, throw an error, or change something in your model.
-Once working, it will most likely be moved to a more permanent location.
+Selects all end caps and taps
 ************************************************************************
 """
 
@@ -55,7 +48,7 @@ ducts = RevitDuct.all(doc, view)
 conical         = [d for d in ducts if d.family == "ConicalTap - wDamper"]
 boot_tap        = [d for d in ducts if d.family == "boot Tap - wDamper"]
 long_coupler    = [d for d in ducts if d.family == "8inch Long Coupler wDamper"]
-long_coupler    = [d for d in ducts if d.family == "8inch Long Coupler wDamper"]
+long_coupler    = [d for d in ducts if d.family == "cap"]
 
 RevitElement.select_many(uidoc, conical + boot_tap + long_coupler)
 
