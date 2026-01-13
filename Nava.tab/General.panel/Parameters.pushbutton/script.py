fb7a8ac629b81b926912544774909d61a90d218f
Jose Nava <heureux13@hotmail.com>
2026-01-13 09:38:13 -0600

    grd tags

--- Nava.tab/General.panel/Parameters.pushbutton/script.py
+++ Nava.tab/General.panel/Parameters.pushbutton/script.py
@@ -39,6 +39,8 @@ output.print_md("---")
 for elid in selected_ids:
     el = doc.GetElement(elid)
     output.print_md("###---- Parameters for Element ID {} ----".format(el.Id))
+
+    # Collect instance parameters
     param_list = []
     for param in el.Parameters:
         try:
@@ -52,11 +54,32 @@ for elid in selected_ids:
                 elif param.StorageType == StorageType.Integer:
                     value = param.AsInteger()
                 elif param.StorageType == StorageType.ElementId:
-
                     value = param.AsElementId()
-            param_list.append((name, value))
+            param_list.append((name, value, "Instance"))
         except Exception as ex:
-            param_list.append((name, "Error - {}".format(ex)))
+            param_list.append((name, "Error - {}".format(ex), "Instance"))
+
+    # Collect type parameters
+    elem_type = doc.GetElement(el.GetTypeId())
+    if elem_type:
+        for param in elem_type.Parameters:
+            try:
+                name = param.Definition.Name
+                value = param.AsString()
+                if value is None:
+                    value = param.AsValueString()
+                if value is None:
+                    if param.StorageType == StorageType.Double:
+                        value = param.AsDouble()
+                    elif param.StorageType == StorageType.Integer:
+                        value = param.AsInteger()
+                    elif param.StorageType == StorageType.ElementId:
+                        value = param.AsElementId()
+                param_list.append((name, value, "Type"))
+            except Exception as ex:
+                param_list.append((name, "Error - {}".format(ex), "Type"))
+
     # Sort and print
-    for name, value in sorted(param_list, key=lambda x: x[0].lower()):
-        output.print_md("**{}**:     *{}*".format(name, value))
+    for name, value, param_type in sorted(param_list, key=lambda x: x[0].lower()):
+        display_value = value if value is not None else "None"
+        output.print_md("**{}** [{}]:     *{}*".format(name, param_type, display_value))
